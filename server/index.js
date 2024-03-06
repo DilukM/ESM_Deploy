4db70719538548de559a79f579fb4384e8263cc6
Diluk Mihiranga <dilukedu@gmail.com>
2024-03-06 11:10:08 +0530

    dot remv

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
+
 import helmet from "helmet";
 import morgan from "morgan";
 import clientRoutes from "./routes/client.js";
@@ -29,7 +29,7 @@ import {
 } from "./data/index.js";
 
 /* CONFIGURATION */
-dotenv.config();
+
 const app = express();
 app.use(express.json());
 app.use(helmet());
