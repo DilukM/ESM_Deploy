d91e69c3c31457b393d12190f57a8c0eb3189c4b
Diluk Mihiranga <dilukedu@gmail.com>
2024-03-20 21:33:02 +0530

    base url set

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,9 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://esm-deploy-server-efc1grnyw-dilukms-projects.vercel.app",
-  }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+
   reducerPath: "adminApi",
   tagTypes: [
     "Donors",
