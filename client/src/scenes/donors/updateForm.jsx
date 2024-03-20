5f4663249f363c179c2be199e3da99d15e6bfe3d
Diluk Mihiranga <dilukedu@gmail.com>
2024-03-20 23:25:21 +0530

    as

--- client/src/scenes/donors/updateForm.jsx
+++ client/src/scenes/donors/updateForm.jsx
@@ -29,7 +29,7 @@ const UpdateForm = ({ open, handleClose, refetch, donorToUpdate }) => {
     }
   }, [donorToUpdate]);
 
-  // const donorId = donorToUpdate ? donorToUpdate._id : "";
+  const donorId = donorToUpdate ? donorToUpdate._id : "";
 
   const handleUpdateDonor = () => {
     updateDonor({ donorToUpdate, name, email, phone, password })
