a80b2d2070e5eec627080e09c1c98ea1d5c95875
Diluk Mihiranga <dilukedu@gmail.com>
2024-08-07 12:35:50 +0530

    console

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,6 +6,7 @@ import { AdminUser } from "../models/admin.js";
 const router = Router();
 
 router.post("/signin", async (req, res) => {
+  console.log(req.body);
   try {
     // Validate request body
     const { error } = validate(req.body);
