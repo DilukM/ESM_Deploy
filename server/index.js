3c59955317892db9897cfc0ae4eeb79a541e5421
Diluk Mihiranga <dilukedu@gmail.com>
2024-08-07 09:38:32 +0530

    Login error fixed

--- server/index.js
+++ server/index.js
@@ -1,21 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+
 import mongoose from "mongoose";
 import cors from "cors";
-import ocr_route from "./routes/ocr_routes.js";
-
 import helmet from "helmet";
 import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 import donorRoutes from "./routes/donor_routes.js";
-
 import dEventRoutes from "./routes/d_events_routes.js";
+import itemsRoutes from "./routes/items_routes.js";
+import items_outRoutes from "./routes/items_out_routes.js";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import treeplantation from "./routes/treeplantation.js";
+import sponsors from "./routes/sponsors.js";
 
 /* CONFIGURATION */
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -37,8 +46,16 @@ app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 app.use("/donors", donorRoutes);
 app.use("/donorevents", dEventRoutes);
-app.use("/ocr", ocr_route);
+app.use("/items", itemsRoutes);
+app.use("/items_out", items_outRoutes);
+app.use("/treePlantationEvent", treeplantation);
+app.use("/sponsors", sponsors);
 
+// Serve static files from the uploads directory
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = 5000 || 9000;
