78c028a19a8d99272661b32e4b012cbf4508a43f
Diluk Mihiranga <dilukedu@gmail.com>
2024-08-14 21:09:45 +0530

    ocr error meaasage change

--- server/controllers/Ocr_Controller.js
+++ server/controllers/Ocr_Controller.js
@@ -2,20 +2,26 @@ import ocr from "../models/Ocr_model.js";
 import jwt from "jsonwebtoken";
 
 export const addocr = async (req, res) => {
-  const { token_No,ID_Number, Name, Address, dob,Issue_Date ,Expiry_Date} = req.body;
+  const { token_No, ID_Number, Name, Address, dob, Issue_Date, Expiry_Date } =
+    req.body;
   try {
     // Check if the donor already exists
     const existingDonor = await ocr.findOne({ ID_Number });
 
-   
     // If donor exists, send error response
     if (existingDonor) {
-      return res.status(400).json({ error: "Donor already exists" });
+      return res.status(400).json({ error: "ID already exists" });
     }
 
     // Create a new donor instance with hashed password
     const newDonor = new ocr({
-      token_No,ID_Number, Name, Address, dob,Issue_Date ,Expiry_Date
+      token_No,
+      ID_Number,
+      Name,
+      Address,
+      dob,
+      Issue_Date,
+      Expiry_Date,
     });
 
     // Save the donor to the database
@@ -23,7 +29,7 @@ export const addocr = async (req, res) => {
 
     // Generate JWT token
     const token = jwt.sign(
-      { Id: newDonor.ID_Number},
+      { Id: newDonor.ID_Number },
       "6474b13e42fd0f0c680c18ab4afa9fbca5e6e9584d98b3bc68617f0b5c8f4249",
       {
         expiresIn: "1h",
@@ -33,13 +39,12 @@ export const addocr = async (req, res) => {
     // Send success response with token
     res.status(200).json({ token });
   } catch (error) {
-    console.error("Error registering donor:", error);
+    console.error("Error registering user:", error);
     res
       .status(500)
       .json({ error: "Registration failed. Please try again later." });
   }
 };
-
 
 export const getocrs = async (req, res) => {
   try {
@@ -59,37 +64,3 @@ export const getocr = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
-
-
-// export const deleteDonors = async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const deletedDonor = await Donors.findByIdAndDelete(id);
-//     if (!deletedDonor) {
-//       return res.status(404).json({ error: "Donor not found" });
-//     }
-//     res.json({ message: "Donor deleted successfully" });
-//   } catch (error) {
-//     console.error("Error deleting donor:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// };
-
-// export const updateDonors = async (req, res) => {
-//   try {
-//     const donorId = req.params.id;
-//     const updatedDonorData = req.body; // Updated donor data from the request body
-
-//     // Find the donor by ID in the database and update its information
-//     const updatedDonor = await Donors.findByIdAndUpdate(
-//       donorId,
-//       updatedDonorData,
-//       { new: true }
-//     );
-
-//     res.json(updatedDonor); // Send back the updated donor object
-//   } catch (error) {
-//     console.error("Error updating donor:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// };
