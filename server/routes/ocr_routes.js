4c0ec0c00a57a9d969711adbd67e3ebcab16fd56
bena <115331768+teshan-nuwantha@users.noreply.github.com>
2024-08-16 03:40:08 +0530

    final edit

--- server/routes/ocr_routes.js
+++ server/routes/ocr_routes.js
@@ -10,8 +10,6 @@ const router = express.Router();
 router.get("/gets", getocrs);
 router.get("/get/:id", getocr);
 router.post("/add", addocr);
-// router.delete("/delete/:id", deleteDonors);
-// router.put("/update/:id", updateDonors);
-// router.post("/login", donorLogin);
+
 
 export default router;
